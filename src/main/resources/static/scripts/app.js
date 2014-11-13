91fe02bd9054f990a4494f2873a96954ba2d99ca
Lauri Harpf <lauri.harpf@acelvia.com>
2014-11-13 20:15:59 +0200

    - Added an ajax spinner for the Wikipedia article (spin.js). Not a very angular (or pretty) way of doing it, but implementing the "show spinner when marker clicked - stop showing when the load event is triggered from the iframe" felt a bit challenging. Good part to refactor at some point?

--- src/main/resources/static/scripts/app.js
+++ src/main/resources/static/scripts/app.js
@@ -58,8 +58,36 @@
                 this.selectedShipwreck = this.shipwrecks[marker.id];
 
                 var wikipediaPage = this.selectedShipwreck.name.replace(/ /g, '_');
+                this.startAjaxSpinner();
                 this.selectedShipwreck.link =
                     $sce.trustAsResourceUrl("http://en.wikipedia.org/wiki/" + wikipediaPage + "?printable=yes");
             };
+
+            this.startAjaxSpinner = function () {
+                var opts = {
+                    lines: 13, // The number of lines to draw
+                    length: 20, // The length of each line
+                    width: 10, // The line thickness
+                    radius: 30, // The radius of the inner circle
+                    corners: 1, // Corner roundness (0..1)
+                    rotate: 0, // The rotation offset
+                    direction: 1, // 1: clockwise, -1: counterclockwise
+                    color: '#000', // #rgb or #rrggbb or array of colors
+                    speed: 1, // Rounds per second
+                    trail: 60, // Afterglow percentage
+                    shadow: false, // Whether to render a shadow
+                    hwaccel: false, // Whether to use hardware acceleration
+                    className: 'spinner', // The CSS class to assign to the spinner
+                    zIndex: 2e9, // The z-index (defaults to 2000000000)
+                    top: '50%', // Top position relative to parent
+                    left: '75%' // Left position relative to parent
+                };
+                this.spinner = new Spinner(opts).spin(document.getElementById("shipwrecks"));
+            };
+
+            // Stop the ajax spinner when the iframe has loaded
+            $("#wikipediaFrame").on("load", function () {
+                controller.spinner && controller.spinner.stop();
+            });
         }]);
 })();
