01092bf5bf6b4443612fb7ef77d1898c15f77155
Lauri Harpf <lauri.harpf@acelvia.com>
2019-12-25 13:54:14 +0200

    Breaking the build

--- src/test/java/com/acelvia/shipwrecks/services/WikipediaShipwreckServiceTest.java
+++ src/test/java/com/acelvia/shipwrecks/services/WikipediaShipwreckServiceTest.java
@@ -9,6 +9,7 @@ import java.io.InputStream;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class WikipediaShipwreckServiceTest {
 
@@ -33,6 +34,7 @@ public class WikipediaShipwreckServiceTest {
 
         @Test
         void returnsExpectedAmountOfShipwrecks() {
+            fail();
             assertEquals(76, shipwrecks.size());
         }
 
