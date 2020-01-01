fb41db6715885409fdbdf49e15ebd48a986d8cd6
Lauri Harpf <lauri.harpf@acelvia.com>
2020-01-01 23:09:48 +0200

    - Remove unused config variables - Fix margin issue

--- src/main/java/com/acelvia/shipwrecks/components/Settings.java
+++ src/main/java/com/acelvia/shipwrecks/components/Settings.java
@@ -9,21 +9,8 @@ public class Settings {
     @Value("${google.id}")
     private String googleId;
 
-    @Value("${google.secret}")
-    private String googleSecret;
-
-    @Value("${google.callback.uri}")
-    private String googleCallbackUri;
-
     public String getGoogleId() {
         return googleId;
     }
 
-    public String getGoogleSecret() {
-        return googleSecret;
-    }
-
-    public String getGoogleCallbackUri() {
-        return googleCallbackUri;
-    }
 }
