358cd84a7b74cc75a335636308491d5dc1ce501c
Lauri Harpf <lauri.harpf@acelvia.com>
2020-04-22 19:51:52 +0300

    - Adds a simple test for one React component. - Attempting to hook that up to the Github CI pipeline, let's see how it goes

---
+++ src/main/js/navigation/__test__/HamburgerMenu.test.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import {
+  cleanup,
+  render,
+  GetByText,
+  RenderResult,
+} from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import HamburgerMenu from "../HamburgerMenu";
+
+describe("HamburgerMenu", () => {
+  afterEach(cleanup);
+
+  const renderMenu = (onlyShowStarred: boolean) =>
+    render(
+      <HamburgerMenu
+        onlyShowStarred={onlyShowStarred}
+        setOnlyShowStarred={() => void false}
+      />
+    );
+
+  describe("switch to only show starred ships", () => {
+    const getSwitch = (result: RenderResult) =>
+      result.getByLabelText("Only show starred");
+
+    it("is not checked when all ships are shown", () => {
+      const result = renderMenu(false);
+      expect(getSwitch(result)).not.toBeChecked();
+    });
+
+    it("is checked when only starred ships are shown", () => {
+      const result = renderMenu(true);
+      expect(getSwitch(result)).toBeChecked();
+    });
+  });
+});
